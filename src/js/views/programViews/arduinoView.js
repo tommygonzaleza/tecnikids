8841250479b4593f6386800db7d9d392efe254f6
tommygonzaleza <tommygonzaleza@users.noreply.github.com>
2020-08-10 21:15:32 -0400

    Ponte en Linea video added, arduino video added.

--- src/js/views/programViews/arduinoView.js
+++ src/js/views/programViews/arduinoView.js
@@ -17,26 +17,38 @@ export const ArduinoView = () => {
                         Education progresan a los estudiantes a través de STEAM desde la 
                         escuela intermedia hasta la universidad, aumentando su complejidad 
                         para desafiarlos a medida que desarrollan sus habilidades.
-                        <br />
-                        <br />
+                    </p>
+                    <div className="col-12 col-md-6 my-3 center-text">
+                        <img className="col-10 col-md-5 mb-4 mb-md-0" src={arduino} alt="Arduino"/>
+                    </div>
+                </div>
+                <div className="row mx-2 mt-2 mt-md-0">
+                    <iframe 
+                        className="col-11 col-md-6 mb-3 mb-md-0"
+                        style={{height: '350px'}}
+                        title="Arduino Video"
+                        src="https://www.youtube.com/embed/Mg7ReUDauGs" 
+                        frameborder="0" 
+                        allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" 
+                        allowfullscreen
+                    >
+                    </iframe>
+                    <p className="col-11 col-md-5 mb-md-3 mb-0 ml-0 ml-md-5 justify-text">
                         Todos los programas incluyen una gama de productos electrónicos como 
                         tableros programables, sensores, piezas mecánicas, software simple de 
                         código abierto, contenido en línea para estudiantes y capacitación guiada 
                         y apoyo para educadores.
-                    </p>
-                    <div className="col-12 col-md-6 my-3 center-text">
-                        <img className="col-10 col-md-7 mb-4 mb-md-5" src={arduino} alt="Arduino"/>
-                        <a className="col-12 my-5" href="https://www.arduino.cc/education">
-                            <img src={button} className="col-9 col-md-7" alt="Button" />
-                        </a>
-                    </div>
-                    <p className="col-11 col-md-12 justify-text">
+                        <br />
+                        <br />
                         Los productos con los que aprenden los estudiantes son los mismos que los 
                         utilizados profesionalmente en empresas de todo el mundo, en aplicaciones 
                         como creación rápida de prototipos, IA, tecnología de drones y aprendizaje 
                         automático.
                     </p>
                 </div>
+                <a className="col-11" href="https://www.arduino.cc/education">
+                    <img src={button} className="col-9 col-md-3 my-2 my-md-5 p-center" alt="Button" />
+                </a>
             </div>
             <SeparationLines />
             <MyFooter />
--- src/js/views/programViews/ponteEnLineaView.js
+++ src/js/views/programViews/ponteEnLineaView.js
@@ -16,7 +16,26 @@ export const PonteEnLineaView = () => {
                         niños y jóvenes es lo mas importante, es por eso que hemos comenzado con 
                         nuestro programa <span className="f-weigth-600">“Ponte en Línea”</span>, una iniciativa que permitirá a los 
                         profesores y alumnos continuar con sus estudios en línea, para seguirse 
-                        formando y así adquirir esos conocimientos tan valiosos. “Ponte en Línea” les 
+                        formando y así adquirir esos conocimientos tan valiosos. 
+                    </p>
+                    
+                    <div className="col-12 col-md-6">
+                        <img src={PonteEnLinea} className="col-12 col-md-10 p-center ml-0 ml-md-5" alt="Ponte En Línea" />
+                    </div>
+                </div>
+                <div className="row mx-2 mt-2">
+                    <iframe 
+                        className="col-11 col-md-6 mx-auto mb-3 mb-md-0"
+                        style={{height: '300px'}}
+                        title="Ponte En Linea Video"
+                        src="https://www.youtube.com/embed/i79PvrUM9Dg" 
+                        frameborder="0" 
+                        allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" 
+                        allowfullscreen
+                    >
+                    </iframe>
+                    <p className="col-11 col-md-6 justify-text">
+                        “Ponte en Línea” les 
                         permitirá realizar evaluaciones, videoconferencias, presentaciones y 
                         discusiones, desde la seguridad del hogar. Estamos comprometidos con la 
                         educación y por ello les ofrecemos el servicio de capacitación de profesores, 
@@ -26,9 +45,6 @@ export const PonteEnLineaView = () => {
                         Con Tecnikids, Transforma tus clases presenciales en lecciones en línea 
                         eficientes. ¡Ahora Mismo! 
                     </p>
-                    <div className="col-12 col-md-6">
-                        <img src={PonteEnLinea} className="col-12 col-md-10 p-center ml-0 ml-md-5 mt-0 mt-md-5 pt-0 pt-md-5" alt="Ponte En Línea" />
-                    </div>
                 </div>
             </div>
             <SeparationLines />
