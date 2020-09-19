58a23579c819c74b084ecffb110865288208bdd9
tommygonzaleza <tommygonzaleza@users.noreply.github.com>
2020-09-19 12:36:11 -0400

    npm run build executed.

--- src/js/components/Colegios.js
+++ src/js/components/Colegios.js
@@ -3,6 +3,7 @@ import Canigua from '../../img/instituciones/logo-canigua.png';
 import LosArcos from '../../img/instituciones/logo-los-arcos.jpg';
 import SimonBolivar from '../../img/instituciones/logo-simon-bolivar.png';
 import Roraima from '../../img/instituciones/logo-roraima.jpg';
+import Semillita from '../../img/instituciones/logo-semillita.png';
 
 export const Colegios = () => {
     return(
@@ -15,6 +16,7 @@ export const Colegios = () => {
                     <a href="https://www.instagram.com/colegiolosarcos/?hl=es-la"><img src={LosArcos} alt="Colegio Los Arcos" className="col-md-2 col-3"/></a>
                     <a href="https://colegiosimonbolivar.edu.ve/"><img src={SimonBolivar} alt="Colegio Simón Bolívar" className="col-md-2 col-4"/></a>
                     <a href="https://www.colegiororaima.com/"><img src={Roraima} alt="Colegio Roraima" className="col-md-3 col-4"/></a>
+                    <a href="https://semillitasunflowers.com/"><img src={Semillita} alt="Preescolar Semillita Sunflowers" className="col-md-2 col-4"/></a>
                 </div>
                 <div className="col-md-1"></div>
             </div>
