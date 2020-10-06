2b4089080ae65c6af21314f76ea908d85f3d3874
tommygonzaleza <tommygonzaleza@users.noreply.github.com>
2020-10-06 14:13:10 -0400

    Step-by-step added to colegios. icon changed.

--- src/js/components/Colegios.js
+++ src/js/components/Colegios.js
@@ -4,6 +4,7 @@ import LosArcos from '../../img/instituciones/logo-los-arcos.jpg';
 import SimonBolivar from '../../img/instituciones/logo-simon-bolivar.png';
 import Roraima from '../../img/instituciones/logo-roraima.jpg';
 import Semillita from '../../img/instituciones/logo-semillita.png';
+import Step from '../../img/instituciones/logo-step-by-step.jpeg';
 
 export const Colegios = () => {
     return(
@@ -17,6 +18,7 @@ export const Colegios = () => {
                     <a href="https://colegiosimonbolivar.edu.ve/"><img src={SimonBolivar} alt="Colegio Simón Bolívar" className="col-md-2 col-4"/></a>
                     <a href="https://www.colegiororaima.com/"><img src={Roraima} alt="Colegio Roraima" className="col-md-3 col-4"/></a>
                     <a href="https://semillitasunflowers.com/"><img src={Semillita} alt="Preescolar Semillita Sunflowers" className="col-md-2 col-4"/></a>
+                    <a href="https://stepbystepkinder.com/"><img src={Step} alt="Step by Step Kinder" className="col-md-2 col-4"/></a>
                 </div>
                 <div className="col-md-1"></div>
             </div>
