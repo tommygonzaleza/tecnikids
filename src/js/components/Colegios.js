49552e12b3872d7717c54497cd1bb8c996557ce5
Tomas Gonzalez <gonzalez.tommy2001@gmail.com>
2021-03-12 17:13:05 +0000

    Personal pages of schools added.

--- src/js/components/Colegios.js
+++ src/js/components/Colegios.js
@@ -12,9 +12,9 @@ export const Colegios = () => {
                     <a href="https://colegiosimonbolivar.edu.ve/"><img src="https://i.ibb.co/JRdCBqH/logo-simon-bolivar.png" alt="Colegio Simón Bolívar" className="col-md-2 col-4"/></a>
                     <a href="https://www.colegiororaima.com/"><img src="https://i.ibb.co/0tkpGL7/logo-roraima.jpg" alt="Colegio Roraima" className="col-md-3 col-4"/></a>
                     <a href="https://www.colegioloscampitos.com/el_colegio.htm"><img src="https://i.ibb.co/60Q5sW5/logo-los-campitos.png" alt="Colegio Los Campitos" className="col-md-2 col-3"/></a>
-                    <a href=""><img src="https://i.ibb.co/wh5N6Pq/logo-step-by-step.jpg" alt="Preescolar Step by Step" className="col-md-2 col-4"/></a>
-                    <a href=""><img src="https://i.ibb.co/LzdPPRX/logo-semillita.png" alt="Preescolar Semillita Sunflower" className="col-md-2 col-3" style={{width: "25vh"}}/></a>
-                    <a href="https://www.eduweb.com.ve/portal/academiamerici/index.php?option=com_jevents&task=modcal.ajax&day=1&month=9&year=2017&modid=91&tmpl=component"><img src="https://i.ibb.co/TK4CPsf/logo-merici-01.png" alt="Academia Merici" className="col-md-2 col-3" style={{width: "24vh"}}/></a>
+                    <a href="https://stepbystepkinder.com/"><img src="https://i.ibb.co/wh5N6Pq/logo-step-by-step.jpg" alt="Kinder Step by Step" className="col-md-2 col-4"/></a>
+                    <a href="https://semillitasunflower.wordpress.com/"><img src="https://i.ibb.co/LzdPPRX/logo-semillita.png" alt="Preescolar Semillita Sunflower" className="col-md-2 col-3" style={{width: "25vh"}}/></a>
+                    <a href="https://www.eduweb.com.ve/portal/academiamerici/index.php?option=com_jevents&task=modcal.ajax&day=1&month=9&year=2017&modid=91&tmpl=component"><img src="https://i.ibb.co/TK4CPsf/logo-merici-01.png" alt="Academia Merici" className="col-md-2 col-3" style={{width: "23vh"}}/></a>
                 </div>
                 <div className="col-md-1"></div>
             </div>
