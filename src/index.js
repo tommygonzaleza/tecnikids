001ec3ac250696cea933275a2d0c4e35fc6abcd2
AbreuY <yolfran12@gmail.com>
2022-02-22 18:27:55 -0500

    Imported neuropol font

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './styles/index.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import * as serviceWorker from './serviceWorker';
 import { Layout } from './js/layout';
+import "./styles/fonts/Neuropol-X-Rg.ttf";
 
 ReactDOM.render(
   <React.StrictMode>
