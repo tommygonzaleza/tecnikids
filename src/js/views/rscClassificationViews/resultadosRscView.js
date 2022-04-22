03ee0483495c384d477e6e6fa78d81e0bb3ef6a2
AbreuY <yolfran12@gmail.com>
2022-04-21 20:13:36 -0500

    Updatedd links for classification results

--- src/js/views/rscClassificationViews/resultadosRscView.js
+++ src/js/views/rscClassificationViews/resultadosRscView.js
@@ -32,32 +32,32 @@ export const rscResultsView = () => {
                 </div>
                 <div className="mt-5">
                     <div className="row mb-3">
-                        <div className="col-4 col-md-3 col-sm-6 col-xl-3 mb-2"> <a href="https://drive.google.com/file/d/1Po3QpnLVYHiAeW-nkYPU9-RmKHx2cnhF/view?usp=drive_web"><img src="https://i.ibb.co/RjgxbmR/Clasificatoria-16.png" className="card-img-top" alt="Colegio Los Arcos" /></a><p className="text-center">COLEGIO <br />LOS ARCOS</p></div>
+                        <div className="col-4 col-md-3 col-sm-6 col-xl-3 mb-2"> <a href="https://i.ibb.co/QmySNdX/losarcos-clasificados.png"><img src="https://i.ibb.co/RjgxbmR/Clasificatoria-16.png" className="card-img-top" alt="Colegio Los Arcos" /></a><p className="text-center">COLEGIO <br />LOS ARCOS</p></div>
                         
-                        <div className="col-4 col-md-3 col-sm-6 col-xl-3 mb-2"><a href="https://drive.google.com/file/d/1_3c4FN-2bUIa5ApN21Vx0un51Xhkg51B/view?usp=sharing" target="_blank"> <img src="https://i.ibb.co/tHxTR3W/Clasificatoria-17.png" className="card-img-top" alt="COLEGIO SIMÓN BOLÍVAR I" /></a><p className="text-center">COLEGIO SIMÓN BOLÍVAR I</p></div>
+                        <div className="col-4 col-md-3 col-sm-6 col-xl-3 mb-2"><a href="https://i.ibb.co/RbPZBxh/SBI-clasificados.png" target="_blank"> <img src="https://i.ibb.co/tHxTR3W/Clasificatoria-17.png" className="card-img-top" alt="COLEGIO SIMÓN BOLÍVAR I" /></a><p className="text-center">COLEGIO SIMÓN BOLÍVAR I</p></div>
                         
-                        <div className="col-4 col-md-3 col-sm-6 col-xl-3 mb-2"><a href="https://drive.google.com/file/d/159iNZY4Jjny_t0KtWo0WHaL8b-s5Kjqh/view?usp=drive_web" target="_blank"> <img src="https://i.ibb.co/tHxTR3W/Clasificatoria-17.png" className="card-img-top" alt="COLEGIO SIMÓN BOLÍVAR II" /></a><p className="text-center">COLEGIO SIMÓN BOLÍVAR II</p></div>
+                        <div className="col-4 col-md-3 col-sm-6 col-xl-3 mb-2"><a href="https://i.ibb.co/bBPTkGM/SBII-clasificados.png" target="_blank"> <img src="https://i.ibb.co/tHxTR3W/Clasificatoria-17.png" className="card-img-top" alt="COLEGIO SIMÓN BOLÍVAR II" /></a><p className="text-center">COLEGIO SIMÓN BOLÍVAR II</p></div>
                         
-                        <div className="col-4 col-md-3 col-sm-6 col-xl-3 mb-2"><a href="https://drive.google.com/file/d/1e46bPUaL3h2pFKM67Wt8UmtC4-EgH4oL/view?usp=drive_web" target="_blank"> <img src="https://i.ibb.co/N31gbLc/Clasificatoria-18.png" className="card-img-top" alt="COLEGIO RORAIMA" /></a><p className="text-center">COLEGIO<br />RORAIMA</p></div>
+                        <div className="col-4 col-md-3 col-sm-6 col-xl-3 mb-2"><a href="https://i.ibb.co/t3VTJcZ/Roraima-clasificados.png" target="_blank"> <img src="https://i.ibb.co/N31gbLc/Clasificatoria-18.png" className="card-img-top" alt="COLEGIO RORAIMA" /></a><p className="text-center">COLEGIO<br />RORAIMA</p></div>
                         
-                        <div className="col-4 col-md-3 col-sm-6 col-xl-3 mb-2"><a href="https://drive.google.com/file/d/1N7hogU1XZV4ttMOP1IiQYRKJGE_QdGOr/view?usp=drive_web" target="_blank"> <img src="https://i.ibb.co/jH8cY8v/Clasificatoria-19.png" className="card-img-top" alt="COLEGIO CANIGUÁ" /></a><p className="text-center">COLEGIO<br />CANIGUÁ</p></div>
+                        <div className="col-4 col-md-3 col-sm-6 col-xl-3 mb-2"><a href="https://i.ibb.co/b3GsYVW/Canigua-clasificados.png" target="_blank"> <img src="https://i.ibb.co/jH8cY8v/Clasificatoria-19.png" className="card-img-top" alt="COLEGIO CANIGUÁ" /></a><p className="text-center">COLEGIO<br />CANIGUÁ</p></div>
                         
-                        <div className="col-4 col-md-3 col-sm-6 col-xl-3 mb-2"><a href="https://drive.google.com/file/d/1IJSYY7nLjlQ_EPC35rSstjAeKCnLQ_fU/view?usp=drive_web" target="_blank"> <img src="https://i.ibb.co/ZhDshXW/Clasificatoria-20.png" className="card-img-top" alt="COLEGIO SAN AGUSTÍN DEL PARAÍSO" /></a><p className="text-center">COLEGIO SAN AGUSTÍN DEL PARAÍSO</p></div>
+                        <div className="col-4 col-md-3 col-sm-6 col-xl-3 mb-2"><a href="https://i.ibb.co/hWsykpZ/San-Agustin-clasificados.png" target="_blank"> <img src="https://i.ibb.co/ZhDshXW/Clasificatoria-20.png" className="card-img-top" alt="COLEGIO SAN AGUSTÍN DEL PARAÍSO" /></a><p className="text-center">COLEGIO SAN AGUSTÍN DEL PARAÍSO</p></div>
 
                         <div className="col-4 col-md-3 col-sm-6 col-xl-3 mb-2"><a href="https://drive.google.com/file/d/1zbhbIOqXmtM1ss_obVefyIkT0Q9XHais/view?usp=sharing" target="_blank"><img src="https://i.ibb.co/Tkfwfwk/Clasificatoria-21.png" className="card-img-top" alt="ACADEMIA MERICI" /></a><p className="text-center">ACADEMIA MERICI</p></div>
                        
                         <div className="col-4 col-md-3 col-sm-6 col-xl-3 mb-2"> <img src="https://i.ibb.co/DkrjZVP/Clasificatoria-22.png" className="card-img-top" alt="COLEGIO ANGELO DI MARTA" /><p className="text-center">COLEGIO ANGELO DI MARTA</p></div>
                        
-                        <div className="col-4 col-md-3 col-sm-6 col-xl-3 mb-2"> <a href="https://drive.google.com/file/d/1NVVvZUFKfx_EuRzio-PSERGaQIgpGXPG/view?usp=drive_web" target="_blank"> <img src="https://i.ibb.co/QkZ0pCG/Clasificatoria-24.png" className="card-img-top" alt="COLEGIO LOS CAMPITOS" /></a><p className="text-center">COLEGIO LOS CAMPITOS</p></div>
+                        <div className="col-4 col-md-3 col-sm-6 col-xl-3 mb-2"> <a href="https://i.ibb.co/JqF9yrw/loscampitos-clasificados.png" target="_blank"> <img src="https://i.ibb.co/QkZ0pCG/Clasificatoria-24.png" className="card-img-top" alt="COLEGIO LOS CAMPITOS" /></a><p className="text-center">COLEGIO LOS CAMPITOS</p></div>
                        
                         <div className="col-4 col-md-3 col-sm-6 col-xl-3 mb-2"> <img src="https://i.ibb.co/kXtn5f3/Clasificatoria-25.png" className="card-img-top" alt="PRE ESCOLAR PUKI PUKI" /><p className="text-center">PRE ESCOLAR PUKI PUKI</p></div>
 
                         <div className="col-4 col-md-3 col-sm-6 col-xl-3 mb-2"> <img src="https://i.ibb.co/BzXwzz8/Clasificatoria-26.png" className="card-img-top" alt="PRE ESCOLAR LOS PINITOS" /><p className="text-center">PRE ESCOLAR LOS PINITOS</p></div>
                         
-                        <div className="col-4 col-md-3 col-sm-6 col-xl-3 mb-2"><a href="https://drive.google.com/file/d/17Mtw3i_MGX2JMtc36iN1Z8CBiJJ6UKoF/view?usp=drive_web" target="_blank"> <img src="https://i.ibb.co/G54GWW4/Clasificatoria-27.png" className="card-img-top" alt="PRE ESCOLAR STEP BY STEP" /></a><p className="text-center">PRE ESCOLAR STEP BY STEP</p></div>
-                        <div className="col-4 col-md-3 col-sm-6 col-xl-3 mb-2"><a href="https://drive.google.com/file/d/1GfaLmuiv_y8yWuKK90mg0AF-EUctUKJ6/view?usp=drive_web" target="_blank"> <img src="https://i.ibb.co/2jP2hB8/Clasificatoria-28.png" className="card-img-top" alt="SUPERATEC" /></a><p className="text-center">SUPERATEC</p></div>
+                        <div className="col-4 col-md-3 col-sm-6 col-xl-3 mb-2"><a href="https://i.ibb.co/0yZgWnk/Stepbystep-clasificados.png" target="_blank"> <img src="https://i.ibb.co/G54GWW4/Clasificatoria-27.png" className="card-img-top" alt="PRE ESCOLAR STEP BY STEP" /></a><p className="text-center">PRE ESCOLAR STEP BY STEP</p></div>
+                        <div className="col-4 col-md-3 col-sm-6 col-xl-3 mb-2"><a href="https://i.ibb.co/Y7pNmcV/Superatec-clasificados.png" target="_blank"> <img src="https://i.ibb.co/2jP2hB8/Clasificatoria-28.png" className="card-img-top" alt="SUPERATEC" /></a><p className="text-center">SUPERATEC</p></div>
                         
-                        <div className="col-4 col-md-3 col-sm-6 col-xl-3 mb-2"><a href="https://drive.google.com/file/d/1LhzevM75Gjna56moQBl-ckFiskyfE0TO/view?usp=drive_web" target="_blank"> <img src="https://i.imgur.com/vm07313.png" className="card-img-top" alt="ALIMENTA LA SOLIDARIA" /></a><p className="text-center">ALIMENTA LA SOLIDARIA</p></div>
+                        <div className="col-4 col-md-3 col-sm-6 col-xl-3 mb-2"><a href="https://i.ibb.co/m00VxB4/Alimentalasolidaridad-clasificados.png" target="_blank"> <img src="https://i.imgur.com/vm07313.png" className="card-img-top" alt="ALIMENTA LA SOLIDARIA" /></a><p className="text-center">ALIMENTA LA SOLIDARIA</p></div>
 
 
                     </div>
@@ -72,7 +72,7 @@ export const rscResultsView = () => {
                             El costo de la entrada general para no competidores será de 20$.</p>
                         <h1 className="font-face-nxrg" style={{ color: "#10418a" }}>2. Lee con atención</h1> <p>el Manual de competidor RSC para conocer todos los detalles:
 
-                            Link: https://drive.google.com/file/d/1qd7Ikq2Of-vg8iLoAXeQNc5tDC6fqNQ3/view?usp=sharing</p>
+                            Link: <a href="https://drive.google.com/file/d/1qd7Ikq2Of-vg8iLoAXeQNc5tDC6fqNQ3/view?usp=sharing" target="_blank">https://drive.google.com/file/d/1qd7Ikq2Of-vg8iLoAXeQNc5tDC6fqNQ3/view?usp=sharing</a></p>
                         <h1 className="font-face-nxrg" style={{ color: "#10418a" }}> 3. Asiste</h1> <p>de manera obligatoria a las capacitaciones correspondientes a tu etapa en la competencia. En el caso de no poder participar en alguna de las fechas, debe ser notificado con tiempo de anticipación:</p>
                         <img src="https://i.imgur.com/4uSNOgm.png" className="card-img-top" />
                         
@@ -81,43 +81,6 @@ export const rscResultsView = () => {
                         <p style={{ color: "#10418a" }}>Aunque no seas competidor, te invitamos a asistir y disfrutar de la experiencia Tecnikids. Compra tu entrada en la sección “Entrada general” a través del siguiente link: <a href="https://tecnikids.winktienda.com" target="_blank">https://tecnikids.winktienda.com</a></p>
                     </div>
                 </div>
-                {/* <div className="row mt-5 pl-3 pl-md-0">
-                    <div className="col">
-                        <p>Los participantes deben programar en <b><i>Python</i></b>, un juego que tiene que consistir en pedirle al usuario una clave secreta para poder ganar.</p>
-
-                        <p>El código tiene que cumplir con las siguientes características:</p>
-                        <ol>
-                            <li> El juego a programar debe solicitar al usuario que introduzca su nombre.</li>
-                            <li>Imprime en pantalla <b><i>un acertijo o pista</i></b>, para que el usuario pueda adivinar la clave correcta (la clave tiene que ser una sola palabra).</li>
-                            <li>Permite al usuario intentar adivinar la palabra 5 veces.</li>
-                            <ul style={{ listStyleType: "none" }}>
-                                <li>- Si en el tercer intento no ha adivinado, imprime en pantalla una <b><i>pista extra.</i></b></li>
-                                <li>- Si el usuario adivina la palabra, se imprime un mensaje personalizado de victoria que incluye el nombre del jugador.</li>
-                                <li>- Si el usuario falla en el quinto intento, se imprime un mensaje personalizado de derrota que incluye el nombre del jugador.</li>
-                            </ul>
-
-                        </ol>
-                        <p><b><i>Nota: Los elementos mencionados anteriormente son los requisitos necesarios para la elaboración del juego,  los detalles extra como validación de datos de entrada, comentarios dentro del código, la creatividad en la temática del juego y las pistas empleadas sumarán puntos en la evaluación.</i></b></p>
-
-                        <p>Se debe entregar el código desarrollado en un archivo de texto (.txt),  esto tiene que ser <b><i>cargado en la plataforma correspondiente al momento de realizar la inscripción.</i></b></p>
-
-                        <div className="text-center mt-5">
-
-                            <h1 className="font-face-nxrg headerText" style={{ color: "#10418a" }}>¡PARTICIPA Y FORMA PARTE DE ESTA GRAN EXPERIENCIA!</h1>
-
-                            <a href="https://forms.gle/cfnuXL7XmJEELXi16" target="_blank" rel="noopener noreferrer"> <img src="https://i.ibb.co/L5WP9RM/Boton-5.png" width="400px" className="img-fluid" alt="Inscríbete" /></a>
-                        </div>
-                    </div>
-                </div>
-                <div className="row mt-5 justify-content-end">
-                    <p className="text-right">
-                        <b>Para más información puedes contactarnos:</b>
-                        <br />
-                        <a href="tel:+58-412-207-0246">+58 412 207 0246</a>
-                        <br />
-                        <a href="mailto:atencionalclientetk@gmail.com">atencionalclientetk@gmail.com</a>
-                    </p>
-                </div> */}
             </div>
         </>
     );
