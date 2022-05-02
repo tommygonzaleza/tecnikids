f024a73a5bced36fb4dde55abd3e68925eecb35a
AbreuY <yolfran12@gmail.com>
2022-05-02 11:18:39 -0500

    Update competitor manual

--- src/js/views/rscClassificationViews/resultadosRscView.js
+++ src/js/views/rscClassificationViews/resultadosRscView.js
@@ -72,7 +72,8 @@ export const rscResultsView = () => {
                             El costo de la entrada general para no competidores será de 20$.</p>
                         <h1 className="font-face-nxrg" style={{ color: "#10418a" }}>2. Lee con atención</h1> <p>el Manual de competidor RSC para conocer todos los detalles:
 
-                            Link: <a href="https://drive.google.com/file/d/1Dp7WBPYUrOkY-wkW3p8nyVFA7RPn_V8a/view" target="_blank">https://drive.google.com/file/d/1Dp7WBPYUrOkY-wkW3p8nyVFA7RPn_V8a/view</a></p>
+                            Link: <a href="https://drive.google.com/file/d/1d5u3NeDcroMVuozR4F1Z28AuveSzCPy4/view?usp=sharing" target="_blank">https://drive.google.com/file/d/1d5u3NeDcroMVuozR4F1Z28AuveSzCPy4/view?usp=sharing
+                            </a></p>
                         <h1 className="font-face-nxrg" style={{ color: "#10418a" }}> 3. Asiste</h1> <p>de manera obligatoria a las capacitaciones correspondientes a tu etapa en la competencia. En el caso de no poder participar en alguna de las fechas, debe ser notificado con tiempo de anticipación:</p>
                         <img src="https://i.ibb.co/SRLqZy7/Cronograma-actividades.png" alt="Cronograma Actividades" className="card-img-top" />
                         
