5b92df03977b8450e06b886376048777ad4c2887
AbreuY <yolfran12@gmail.com>
2022-08-21 13:09:16 -0500

    [Added] :hammer: more colleges

--- src/js/components/Colegios.js
+++ src/js/components/Colegios.js
@@ -3,7 +3,7 @@ import React from 'react';
 export const Colegios = () => {
     return(
         <div className="container my-5">
-            <h2 className="col-6 text-center">ALIADOS</h2>
+            <h2 className="col-6 text-center">COLEGIOS ALIADOS</h2>
             <div className="row w-auto my-3">
                 <div className="col-md-1"></div>
                 <div className="col-12">
@@ -18,11 +18,24 @@ export const Colegios = () => {
                     <a href="https://stepbystepkinder.com/"><img src="https://i.ibb.co/wh5N6Pq/logo-step-by-step.jpg" alt="Kinder Step by Step" className="col-md-3 col-4"/></a>
                     <a href="http://www.colegioitaloplc.edu.ve/es/"><img src="https://i.ibb.co/YhFg9F2/Italo.jpg" alt="C.I.V. Angelo De Marta" className="col-md-2 col-4"/></a>
                     <a href="http://www.lospinitos.net/familia-pinitos/"><img src="https://i.ibb.co/jHJWM3M/logo-pinitos-2017.png" alt="Los Pinitos" className="col-md-2 col-4"/></a>
-                    <a href="#"><img src="https://i.ibb.co/VQsZm40/instituto-escuela.png" alt="Instituto Escuela" className="col-md-2 col-4"/></a>
-                    <a href="#"><img src="https://i.ibb.co/z7pmrq9/Logo-Cristo-Rey.png" alt="Cristo Rey" className="col-md-2 col-4"/></a>
+                    <a href="#"><img src="https://i.ibb.co/z7pmrq9/Logo-Cristo-Rey.png" alt="Cristo Rey" className="col-md-2 col-4" height='200px'/></a>
                     <a href="#"><img src="https://i.ibb.co/ZHDQ05r/logo-cumbres-circulo.png" alt="Instituto Cumbres" className="col-md-2 col-4"/></a>
                     <a href="#"><img src="https://i.ibb.co/X2thkTW/LOGO-CEIMONTE-ALTO.png" alt="Monte Alto" className="col-md-2 col-4"/></a>
                     <a href="#"><img src="https://i.ibb.co/1RQ8tkQ/logo-academia-washington.png" alt="Academia Whashington" className="col-md-2 col-4"/></a>
+                    <a href="#"><img src="https://i.ibb.co/64K2GD3/colegioaltamiraescudo.png" alt="Colegio Altamira" className="col-md-2 col-4"/></a>
+                    <a href="#"><img src="https://i.ibb.co/LRxVNFP/Colegio-Laura-Vicun-a-1.jpg" alt="Colegio Laura Vicuña" className="col-md-2 col-4"/></a>
+                    <a href="#"><img src="https://i.ibb.co/bNSBxsN/Logo-LAS-TUNITAS-01-1.png" alt="Las Tunitas" className="col-md-2 col-4"/></a>
+                    <a href="#"><img src="https://i.ibb.co/BtFF5Xv/btitish.png" alt="The British School" className="col-md-2 col-4"/></a>
+                    <a href="#"><img src="https://i.ibb.co/9Vy3Lhx/Logo-Colegio-Madre-Matilde.png" alt="Colegio Madre Matilde" className="col-md-2 col-4"/></a>
+                    <a href="#"><img src="https://i.ibb.co/rkYzMCB/logo-CEAMM.png" alt="CEAMM" className="col-md-2 col-4"/></a>
+                    <a href="#"><img src="https://i.ibb.co/xjppgHf/Colegio-San-Agustin-El-Paraiso.png" alt="Colegio San Agustin del Paraiso" className="col-md-2 col-4"/></a>
+                    <a href="#"><img src="https://i.ibb.co/mB5Ksyy/Mater-Mcbo-color-y-azul.png" alt="Mater Salvatoris" className="col-md-2 col-4"/></a>
+                    <a href="#"><img src="https://i.ibb.co/SKBDR6L/Caracas-La-Colina-01-color-1-1.png" alt="La Salle La Colina" className="col-md-2 col-4"/></a>
+                    <a href="#"><img src="https://i.ibb.co/dD4sYL5/unnamed-3.jpg" alt="MS" className="col-md-2 col-4"/></a>
+                    <a href="#"><img src="https://i.ibb.co/jfTFDdJ/Henry-Clay.png" alt="Henry Clay" className="col-md-2 col-4"/></a>
+                    <a href="#"><img src="https://i.ibb.co/59YW7HV/Logo-Don-Bosco-02-02.png" alt="Don Bosco" className="col-md-2 col-4"/></a>
+                    <a href="#"><img src="https://i.ibb.co/54LRtTB/Colegio-Instituto-Maestro-Simon.png" alt="Colegio I Maestro Simon" className="col-md-2 col-4"/></a>
+                    <a href="#"><img src="https://i.ibb.co/6whymdK/Colegio-Instituto-Andes.png" alt="Colegio I Andes" className="col-md-2 col-4"/></a>
                </div>
                 <div className="col-md-1"></div>
             </div>
