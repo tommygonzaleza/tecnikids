b36568077348ab41b149b51546f493b69f4e7086
devAbreu <yolfrank.abreu@gmail.com>
2022-09-17 00:47:20 -0500

    [MODIFIED] :hammer: font type and icons

--- src/js/components/Stadistics.js
+++ src/js/components/Stadistics.js
@@ -6,55 +6,55 @@ export const Stadistics = () => {
         <div className="container-fluid stadistics">
             <div className="row mx-0">
                 <div className="col-10 col-md-2 m-3 mx-auto m-md-5">
-                    <img src="https://i.ibb.co/dWzpQLp/date-white.png" alt="+20 Años" className="stadistics-icon col-8 p-center my-2" />
+                    <img src="https://i.ibb.co/JCrY15b/iconos-web-10-anos.png" alt="+20 Años" className="stadistics-icon col-8 p-center my-2" />
                     <div className="col-12 center-text">
                         <CountUp
-                            className="stadistics-title"
+                            className="stadistics-title font-face-gothamrnd-bold"
                             delay={2}
                             end={3}
                             duration={7}
                         />
                     </div>
-                    <p className="stadistics-text">Años</p>
+                    <p className="stadistics-text font-face-gothamrnd-bold">Años</p>
                 </div>
                 <div className="col-10 col-md-2 m-3 mx-auto m-md-5">
-                    <img src="https://i.ibb.co/SmzvVdF/school-white.png" alt="+210 Colegios" className="stadistics-icon col-8 p-center my-2" />
+                    <img src="https://i.ibb.co/CHTRjJ7/iconos-web-13-colegios.png" alt="+210 Colegios" className="stadistics-icon col-8 p-center my-2" />
                     <div className="col-12 center-text">
                         <CountUp
-                            className="stadistics-title"
+                            className="stadistics-title font-face-gothamrnd-bold"
                             delay={2}
                             end={34}
                             duration={7}
                         />
                     </div>
                     
-                    <p className="stadistics-text">Colegios</p>
+                    <p className="stadistics-text font-face-gothamrnd-bold">Colegios</p>
                 </div>
                 <div className="col-10 col-md-2 m-3 mx-auto m-md-5">
-                    <img src="https://i.ibb.co/fDMLMtm/student-white.png" alt="+25.000 Estudiantes" className="stadistics-icon col-8 p-center my-2" />
+                    <img src="https://i.ibb.co/FBGWWpx/iconos-web-12-estudiantes.pngg" alt="+25.000 Estudiantes" className="stadistics-icon col-8 p-center my-2" />
                     <div className="col-12 center-text">
                         <CountUp
-                            className="stadistics-title"
+                            className="stadistics-title font-face-gothamrnd-bold"
                             delay={2}
                             end={14000}
                             duration={7}
                         />
                     </div>
                     
-                    <p className="stadistics-text">Estudiantes</p>
+                    <p className="stadistics-text font-face-gothamrnd-bold">Estudiantes</p>
                 </div>
                 <div className="col-10 col-md-2 m-3 mx-auto m-md-5">
-                    <img src="https://i.ibb.co/dJr02K8/teacher-white.png" alt="+60 Profesores" className="stadistics-icon col-8 p-center my-2" />
+                    <img src="https://i.ibb.co/vk89Bsg/iconos-web-11-estados.png" alt="+60 Profesores" className="stadistics-icon col-8 p-center my-2" />
                     <div className="col-12 center-text">
                         <CountUp
-                            className="stadistics-title"
+                            className="stadistics-title font-face-gothamrnd-bold"
                             delay={2}
                             end={4}
                             duration={7}
                         />
                     </div>
                     
-                    <p className="stadistics-text">Estados</p>
+                    <p className="stadistics-text font-face-gothamrnd-bold">Estados</p>
                 </div>
             </div>
         </div>
