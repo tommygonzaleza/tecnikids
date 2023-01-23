934f857e843860339a31df6faa088795235787f1
AbreuY <yolfran12@gmail.com>
2023-01-23 10:41:19 -0500

    :hammer: added slideview

---
+++ src/js/views/slideView.js
@@ -0,0 +1,116 @@
+import React from 'react';
+import { Carousel } from 'react-bootstrap';
+
+export const SlideView = () => {
+    return (
+        <div className="container-fluid">
+            <Carousel className="my-carousel">
+                <Carousel.Item className="my-carousel-item">
+                    <img
+                        className="my-carousel-image d-block w-100"
+                        src="https://i.ibb.co/FwBM1cn/RSC-Tecnikids11.jpg"
+                        alt="¡Innovando la educación en Venezuela!"
+                    />
+
+                </Carousel.Item>
+                <Carousel.Item className="my-carousel-item">
+                    <img
+                        className="my-carousel-image d-block w-100"
+                        src="https://i.ibb.co/yd0VY3v/RSC-Tecnikids14.jpg"
+                        alt="Ofrecemos Porgramas Educativos"
+                    />
+
+                </Carousel.Item>
+                <Carousel.Item className="my-carousel-item">
+                    <a href="/rsc-resultados"><img
+                        className="my-carousel-image d-block w-100"
+                        src="https://i.ibb.co/GnMcfGw/RSC-Tecnikids18.jpg"
+                        alt="Torneo de Robótica"
+                    /> </a>
+
+
+                </Carousel.Item>
+                <Carousel.Item className="my-carousel-item">
+                    <a href="/courses-view"><img
+                        className="my-carousel-image d-block w-100"
+                        src="https://i.ibb.co/NKK3M9m/RSC-Tecnikids33.jpg"
+                        alt="Cursos Online"
+                    /> </a>
+
+
+                </Carousel.Item>
+                <Carousel.Item className="my-carousel-item">
+                    <a href="/academy-view"><img
+                        className="my-carousel-image d-block w-100"
+                        src="https://i.ibb.co/RNQXsww/RSC-Tecnikids48.jpg"
+                        alt="Plan Vacacional"
+                    /> </a>
+                </Carousel.Item>
+                <Carousel.Item className="my-carousel-item">
+                    <a href="/academy-view"><img
+                        className="my-carousel-image d-block w-100"
+                        src="https://i.ibb.co/XkS7PzN/RSC-Tecnikids52.jpg"
+                        alt="Plan Vacacional"
+                    /> </a>
+                </Carousel.Item>
+                <Carousel.Item className="my-carousel-item">
+                    <a href="/academy-view"><img
+                        className="my-carousel-image d-block w-100"
+                        src="https://i.ibb.co/JkY6RBL/RSC-Tecnikids64.jpg"
+                        alt="Plan Vacacional"
+                    /> </a>
+                </Carousel.Item>
+                <Carousel.Item className="my-carousel-item">
+                    <a href="/academy-view"><img
+                        className="my-carousel-image d-block w-100"
+                        src="https://i.ibb.co/28bMPLz/RSC-Tecnikids65.jpg"
+                        alt="Plan Vacacional"
+                    /> </a>
+                </Carousel.Item>
+                <Carousel.Item className="my-carousel-item">
+                    <a href="/academy-view"><img
+                        className="my-carousel-image d-block w-100"
+                        src="https://i.ibb.co/w6jbjnS/RSC-Tecnikids74.jpg"
+                        alt="Plan Vacacional"
+                    /> </a>
+                </Carousel.Item>
+                <Carousel.Item className="my-carousel-item">
+                    <a href="/academy-view"><img
+                        className="my-carousel-image d-block w-100"
+                        src="https://i.ibb.co/RBXLx8H/RSC-Tecnikids79.jpg"
+                        alt="Plan Vacacional"
+                    /> </a>
+                </Carousel.Item>
+                <Carousel.Item className="my-carousel-item">
+                    <a href="/academy-view"><img
+                        className="my-carousel-image d-block w-100"
+                        src="https://i.ibb.co/tJZjRNj/RSC-Tecnikids81.jpg"
+                        alt="Plan Vacacional"
+                    /> </a>
+                </Carousel.Item>
+                <Carousel.Item className="my-carousel-item">
+                    <a href="/academy-view"><img
+                        className="my-carousel-image d-block w-100"
+                        src="https://i.ibb.co/vVLHXSw/RSC-Tecnikids82.jpg"
+                        alt="Plan Vacacional"
+                    /> </a>
+                </Carousel.Item>
+                <Carousel.Item className="my-carousel-item">
+                    <a href="/academy-view"><img
+                        className="my-carousel-image d-block w-100"
+                        src="https://i.ibb.co/cFgj37B/RSC-Tecnikids86.jpg"
+                        alt="Plan Vacacional"
+                    /> </a>
+                </Carousel.Item>
+                <Carousel.Item className="my-carousel-item">
+                    <a href="/academy-view"><img
+                        className="my-carousel-image d-block w-100"
+                        src="https://i.ibb.co/VT6c0xX/RSC-Tecnikids87.jpg"
+                        alt="Plan Vacacional"
+                    /> </a>
+                </Carousel.Item>
+            </Carousel>
+        </div>
+
+    )
+}
