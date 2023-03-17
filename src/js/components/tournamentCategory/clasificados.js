ff1d58aba728b01ea2b29c53451d0bfb3be8eccb
jose0109 <martinezujd@gmail.com>
2023-03-17 12:44:32 -0400

    Add tournament ticket component

---
+++ src/js/components/tournamentCategory/clasificados.js
@@ -0,0 +1,25 @@
+{
+    school: "Colegio Don Bosco",
+    category: {
+      "Little space programmers": [
+        "Andres Mills",
+        "Celeste John",
+        "Fabianna Valentina Maita Martinez",
+      ],
+      "Little space engineers": [
+        "Christofer Dvila Rodrigues",
+        "Cynthia John",
+        "Jesús Andrés Sarmiento Moreno",
+      ],
+      "NASA technicians": [
+        "Gabriel José Faneite Olivet",
+        "Israel García",
+        "Juan Diego Casares Castro",
+      ],
+      "NASA Specialists": [
+        "Freddy Alejandro Fernández Tovar",
+        "Gabriel José Fonseca Ferreira",
+        "Luis Miguel Velásquez Ramírez",
+      ],
+    },
+  },
