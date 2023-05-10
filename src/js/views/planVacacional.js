773d9d4cad2ee7760216d4f17f9db576f1b2de33
jose0109 <martinezujd@gmail.com>
2023-05-10 18:08:52 -0400

    Add vacational plan

--- src/js/views/planVacacional.js
+++ src/js/views/planVacacional.js
@@ -1,151 +1,196 @@
-import React from 'react';
-import '../../styles/index.css';
-import { SeparationLines } from '../components/SeparationLines';
-import { Carousel } from 'react-bootstrap';
+import React from "react";
+import "../../styles/index.css";
+import { SeparationLines } from "../components/SeparationLines";
+import { Carousel } from "react-bootstrap";
+import styles from "./PlanVacacional.module.css";
+import vacationalPlan from "../../vacationalPlan.mp4";
+import ReactPlayer from "react-player";
 
 export const planVacacional = () => {
-    return (
-        <div className="container-fluid">
-            <SeparationLines />
-            <div className="container">
-                <div className="row align-items-lg-center">
-                    <div className="col-12 col-md-6 col-lg-6 col-xl-6 text-center">
-                        <h1 style={{ color: "#10418a" }}><b className="font-face-cocogoose">¡CUPOS LIMITADOS!</b></h1>
-                        <p>Información general:<br /><br />
-                            Fecha: 1 al 26 de agosto 2022<br />
-                            Edades: 2 - 12 años.<br />
-                            Modalidad: Presencial.<br />
-                        </p>
-                    </div>
-                    <div className="col-12 col-md-6 col-lg-6 col-xl-6">
-                        <img src="https://i.ibb.co/N2k9TpH/Logo-PVT2021-copia.png" className="img-fluid" style={{ minWidth: "200px", minHeight: "200px" }} />
-                    </div>
-                </div>
-                <div className="row">
-                    <div className="col p-5">
-                        <Carousel className="my-carousel">
-                            <Carousel.Item className="my-carousel-item">
-                                <img
-                                    className="my-carousel-image d-block w-100"
-                                    src="https://i.ibb.co/rQBcSk1/IMG-6716copia.jpg"
-                                    alt="Plan Vacacional"
-                                />
-                            </Carousel.Item>
-                            <Carousel.Item className="my-carousel-item">
-                                <img
-                                    className="my-carousel-image d-block w-100"
-                                    src="https://i.ibb.co/T8wmV8w/IMG-6716-1-copia.jpg"
-                                    alt="Plan Vacacional"
-                                />
-                            </Carousel.Item>
-                            <Carousel.Item className="my-carousel-item">
-                                <img
-                                    className="my-carousel-image d-block w-100"
-                                    src="https://i.ibb.co/G2M30jF/IMG-6716-1.jpg"
-                                    alt="Plan Vacacional"
-                                />
-                            </Carousel.Item>
-                            <Carousel.Item className="my-carousel-item">
-                                <img
-                                    className="my-carousel-image d-block w-100"
-                                    src="https://i.ibb.co/jwkZcYb/carousel-image.png"
-                                    alt="Plan Vacacional"
-                                />
-                                
-                            </Carousel.Item>
-                            <Carousel.Item className="my-carousel-item">
-                                <img
-                                    className="my-carousel-image d-block w-100"
-                                    src="https://i.ibb.co/RvxYq3j/carousel-image-2.png"
-                                    alt="Plan Vacacional"
-                                />
-                            </Carousel.Item>
-                            <Carousel.Item className="my-carousel-item">
-                                <img
-                                    className="my-carousel-image d-block w-100"
-                                    src="https://i.ibb.co/FqZG8QW/carousel-image-3.png"
-                                    alt="Plan Vacacional"
-                                />
-                            </Carousel.Item>
-                            <Carousel.Item className="my-carousel-item">
-                                <img
-                                    className="my-carousel-image d-block w-100"
-                                    src="https://i.ibb.co/hLp8LkP/carouse-image-4.png"
-                                    alt="Plan Vacacional"
-                                />
-                            </Carousel.Item>
-                        </Carousel>
-                    </div>
-                </div>
-                <div className="row">
-                    <div className="col">
-                        <img src="https://i.ibb.co/g3dBMcS/MOCKUP-2.png" width="100%" />
-                    </div>
-                </div>
-                <span className="d-flex justify-content-center"><hr className="w-25" /></span>
-                <div className="row">
-                    <div className="col">
-                        <h1 className="display-3 text-center font-weight-bold font-face-cocogoose" style={{ color: "#10418a" }}> EXPERIENCIAS TECNIKIDS </h1>
-                    </div>
-                </div>
-                <div className="row">
-                    <div className="col-lg-6 col-xl-6 mb-2">
-                        <img src="https://i.ibb.co/vL19zs9/IMG-6557.jpg" width="100%" />
-                        <p className="font-weight-bold" style={{ color: "#10418a" }}>Programación de robots</p>
-                    </div>
-                    <div className="col-lg-6 col-xl-6 mb-2">
-                        <img src="https://i.ibb.co/z7MMQkY/IMG-4151-1.jpg" width="100%" />
-                        <p className="font-weight-bold" style={{ color: "#10418a" }}>Armados K'Nex</p>
-                    </div>
-                    <div className="col-lg-6 col-xl-6 mb-2">
-                        <img src="https://i.ibb.co/ctHtMGH/951cf775-aad1-4071-86da-293655622968-copia.jpg" width="100%" />
-                        <p className="font-weight-bold" style={{ color: "#10418a" }}>Vuelo de drones</p>
-                    </div>
-                    <div className="col-lg-6 col-xl-6 mb-2">
-                        <img src="https://i.ibb.co/7kFMqKr/951cf775-aad1-4071-86da-293655622968.jpg" width="100%" />
-                        <p className="font-weight-bold" style={{ color: "#10418a" }}>Programación de videojuegos con Scratch!</p>
-                    </div>
-                    <div className="col-lg-6 col-xl-6 mb-2">
-                        <img src="https://i.ibb.co/0MZpWcQ/f6901ca6-6a77-4eee-8eeb-c89990c93631.jpg" width="100%" />
-                        <p className="font-weight-bold" style={{ color: "#10418a" }}>Impresiones en 3D</p>
-                    </div>
-                    <div className="col-lg-6 col-xl-6 mb-2">
-                        <img src="https://i.ibb.co/5x2nRdp/Sin-t-tulo-1.jpg" width="100%" />
-                        <p className="font-weight-bold" style={{ color: "#10418a" }}>Actividades recreativas y más sorpresas!</p>
-                    </div>
-                </div>
-                <div className="row mb-3">
-                    <div className="col-lg-6 col-xl-6 text-center">
-                        <i class="bi bi-geo-alt-fill boostrap-icon"></i>
-                        <p className="font-weight-bold" style={{ color: "#10418a" }}>Av. El Morao Calle Porlamar, Quinta Juan Pablo II - El Cafetal</p>
-                    </div>
-                    <div className="col-lg-6 col-xl-6 text-center">
-                        <i class="bi bi-telephone-fill boostrap-icon"></i>
-                        <p className="font-weight-bold" style={{ color: "#10418a" }}>+58 412 207 0246
-                            <br />
-                            atencionalclientetk@gmail.com</p>
-                    </div>
-                </div>
-                <div className="row">
-                    <div className="col text-center">
-                        <h2 className="font-weight-bold font-face-cocogoose" style={{ color: "#10418a" }}>PROCESO DE INSCRIPCIÓN</h2>
-                        <p>Pasos a seguir:</p>
-                        <p>1. Realice el pago correspondiente a Tecnikids Venezuela (Depósito Bancario o transferencia). Los datos se enviarán luego de ser llenado el formulario.</p>
-                        <p>2. Llene el formulario de <a href="https://docs.google.com/forms/d/1w0WxY95xJGQ1ocAFjlpL1WwSgfsngMFtAPiIqXm-jI0/edit?usp=drive_web" target="_blank"><span style={{ color: "#10418a" }}>"Planilla de Inscripción Plan Vacacional 2022".</span></a> Los campos obligatorios están señalados por un asterisco, lo que impide que se continúe con el proceso de inscripción en caso de no contestarlos.</p>
-                        <p>3. Debe enviar el comprobante de pago al correo <a href="mailto:atencionalclientetk@gmail.com"><span style={{ color: "#10418a" }}>atencionalclientetk@gmail.com</span></a> para culminar con el proceso de inscripción y asegurar el cupo del participante. En el asunto debe colocar "Comprobante de pago" y el nombre del campista (OBLIGATORIO).</p>
-                        <p>4. El gasto de cualquier accidente ocurrido, durante el Plan Vacacional, es responsabilidad de los padres o representantes.</p>
-                        <p>5. En caso que por situaciones ajenas no se pueda organizar el plan vacacional se considerará hacerlo de forma virtual.</p>
-                        <p>6. Al finalizar el proceso de inscripción, descargar <a href="https://drive.google.com/file/d/1yfkJY27mNADKtJ3XR0GTccTtL_u1t31P/view?usp=sharing" target="_blank"><span style={{ color: "#10418a" }}>Planilla Aceptación de Condiciones"</span></a> e imprimir para llevarla firmada el primer día.</p>
-
-                        <p>En caso de que una vez iniciado el Plan Vacacional, el campista decida abandonarlo por cualquier motivo, incluso con la autorización de los padres o tutores, no tendrá derecho a ninguna devolución.</p>
-
-                        <p>LA INSCRIPCIÓN NO SERÁ CONSIDERADA COMPLETA, HASTA NO CUMPLIR CON TODOS LOS REQUISITOS ANTERIORMENTE SOLICITADOS. LES RECORDAMOS QUE LA INSCRIPCIÓN DEBE ESTAR CANCELADA EN SU TOTALIDAD.</p>
-                    </div>
-                </div>
-            </div>
-            <SeparationLines />
-
+  return (
+    <div className="container-fluid">
+      <SeparationLines />
+      <div className="container">
+        <div className="row align-items-lg-center mx-0">
+          <div className={`container-fluid ${styles["plan-logo"]}`}>
+            <img
+              src="https://i.ibb.co/bKVfCNt/Logo-Plan-Vacacional-modif-transp-1.png"
+              alt="Logo-Plan-Vacacional-modif-transp-1"
+              border="0"
+            />
+          </div>
+          <Carousel>
+            <Carousel.Item interval={16000}>
+              <div className="player-wrapper  d-block">
+                <ReactPlayer
+                  className="react-player"
+                  url={vacationalPlan}
+                  width="100%"
+                  height="100%"
+                  loop={true}
+                  playing={true}
+                />
+              </div>
+            </Carousel.Item>
+          </Carousel>
         </div>
-    );
+        <div className={styles["plan-info"]}>
+          <h1 className="display-4 text-center font-weight-bold font-face-cocogoose">
+            ¡El aprendizaje se potencia con más diversión!
+          </h1>
+          <h3 className="display-5 font-weight-light font-face-poppins-regular">
+            Para niños de 2 a 12 años:
+          </h3>
+        </div>
 
+        <div className={styles["plan-cta"]}>
+          <a href="https://forms.gle/Y4zUM8hG1RrY5i7T9">
+            <div className={styles["plan-cta-item"]}>
+              <div className={styles.bold}>Temporada 1:</div>{" "}
+              <div>del 7 al 11 de agosto</div>
+            </div>
+          </a>
+          <a href="https://forms.gle/Y4zUM8hG1RrY5i7T9">
+            <div className={styles["plan-cta-item"]}>
+              <div className={styles.bold}>Temporada 2:</div>{" "}
+              <div>del 14 al 18 de agosto</div>
+            </div>
+          </a>
+        </div>
+        <div className={styles["plan-details"]}>
+          <div>
+            <div className={styles.bold}>Horario:</div>{" "}
+            <div>8:00 AM a 1:00 PM</div>
+          </div>
+          <div>
+            <div className={styles.bold}>Ubicación:</div>{" "}
+            <div>
+              Av. El Mora, Calle Porlamar, Quinta Juan Pablo II, El Cafetal.
+              Caracas, Miranda.
+            </div>
+          </div>
+        </div>
+        <div className={styles.division}></div>
+        <div className="row mx-0">
+          <div className={`container-fluid ${styles["plan-logo"]}`}>
+            <img
+              src="https://i.ibb.co/NY2GmL3/MOCKUP-PAGINA-plan-vacacional2.png"
+              alt="MOCKUP-PAGINA-plan-vacacional2"
+              border="0"
+            />
+          </div>
+        </div>
+        <div className={`${styles["plan-info"]} ${styles["academy"]}`}>
+          <h1 className="display-4 text-center font-weight-bold font-face-cocogoose">
+            ¡El aprendizaje se potencia con más diversión!
+          </h1>
+          <h3 className="display-5 font-weight-light font-face-poppins-regular text-align-center">
+            El Intensivo de Academia Tecnikids Venezuela tiene como concepto
+            formar a jovenes en el ambito de la robotica y programacion durante
+            el periodo vacacional para estudiantes de 13 a 18 años.
+          </h3>
+          <h3 className="display-5 font-weight-light font-face-poppins-regular">
+            Buscamos desarrollar las habilidades para el futuro, concentrándose
+            el aprendizaje sobre electrónica y programación sin perder la
+            diversión de verano.
+          </h3>
+        </div>
+
+        <div className={`${styles["plan-cta"]} ${styles["academy-cta"]}`}>
+          <a href="https://forms.gle/QmudGoxqQvmrP6vJ7">
+            <div className={styles["academy-cta-item"]}>
+              <div className={styles.bold}>Temporada 1:</div>{" "}
+              <div>del 7 al 11 de agosto</div>
+            </div>
+          </a>
+          <div>
+            <a href="https://forms.gle/QmudGoxqQvmrP6vJ7">
+              <div className={styles["academy-cta-item"]}>
+                <div className={styles.bold}>Temporada 2:</div>{" "}
+                <div>del 14 al 18 de agosto</div>
+              </div>
+            </a>
+          </div>
+        </div>
+        <div className={`${styles["plan-details"]} ${styles["academy"]}`}>
+          <div>
+            <div className={styles.bold}>Horario:</div>{" "}
+            <div>9:30 AM a 12:30 PM</div>
+          </div>
+          <div>
+            <div className={styles.bold}>Ubicación:</div>{" "}
+            <div>
+              Oficentro los Ruices (Piso 2 oficina P2F, la penúltima a la
+              izquierda). Av Diego Cisneros, Los Ruices. Justo antes del
+              distribuidor de la autopista.
+            </div>
+          </div>
+        </div>
+        <div className={styles.division}></div>
+        <div className="row mx-0">
+          <div className={styles["register-info"]}>
+            <div className="">
+              <img
+                src="https://i.ibb.co/V3fSNVh/Metodos-de-pago-2.png"
+                alt="Metodos-de-pago-2"
+                border="0"
+              />
+            </div>
+            <div className="text-justify px-4 mx-5">
+              <h2
+                className="font-weight-bold font-face-cocogoose"
+                style={{ color: "#10418a" }}
+              >
+                PROCESO DE INSCRIPCIÓN
+              </h2>
+              <p>Pasos a seguir:</p>
+              <p>
+                1. Realice el pago correspondiente a Tecnikids Venezuela
+                (Depósito Bancario o transferencia). Los datos se enviarán luego
+                de ser llenado el formulario.
+              </p>
+              <p>
+                2. Llene el formulario de{" "}
+                <a
+                  href="https://docs.google.com/forms/d/1w0WxY95xJGQ1ocAFjlpL1WwSgfsngMFtAPiIqXm-jI0/edit?usp=drive_web"
+                  target="_blank"
+                >
+                  <span style={{ color: "#10418a" }}>
+                    "Planilla de Inscripción Academia - Tecnikids VE 2022-2023".
+                  </span>
+                </a>{" "}
+                Los campos obligatorios están señalados por un asterisco, lo que
+                impide que se continúe con el proceso de inscripción en caso de
+                no contestarlos.
+              </p>
+              <p>
+                3. Debe enviar el comprobante de pago al correo{" "}
+                <a href="mailto:academia.tecnikidsve@gmal.com">
+                  <span style={{ color: "#10418a" }}>
+                    academia.tecnikidsve@gmal.com
+                  </span>
+                </a>{" "}
+                para culminar con el proceso de inscripción y asegurar el cupo
+                del participante. En el asunto debe colocar "Comprobante de
+                pago" y el nombre del estudiante (OBLIGATORIO).
+              </p>
+              <p>Inscripciones abiertas a partir de Mayo 2023.</p>
+
+              <p>
+                LA INSCRIPCIÓN NO SERÁ CONSIDERADA COMPLETA, HASTA NO CUMPLIR
+                CON TODOS LOS REQUISITOS ANTERIORMENTE SOLICITADOS. LES
+                RECORDAMOS QUE LA INSCRIPCIÓN DEBE ESTAR CANCELADA EN SU
+                TOTALIDAD.
+              </p>
+              <p>
+                Para mas información puedes contactarnos Tecnikids Venezuela +58
+                412 207 0246 atencionalclientetk@gmail.com
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <SeparationLines />
+    </div>
+  );
 };
