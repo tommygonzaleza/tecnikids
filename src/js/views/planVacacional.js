eac8c9cb28d0a4fdaf6bda76665f529cdcfa6109
jose0109 <martinezujd@gmail.com>
2023-05-13 09:29:31 -0400

    Change YT video and fix typos

--- src/js/views/planVacacional.js
+++ src/js/views/planVacacional.js
@@ -31,7 +31,7 @@ export const planVacacional = () => {
           </div>
         </div>
         <div className={styles["player-wrapper"]}>
-          <YouTube videoId="jAL5ptBLgXw" opts={opts} />
+          <YouTube videoId="Wd8_IiNpv10" opts={opts} />
         </div>
         <div className={styles["plan-info"]}>
           <h1 className="display-4 text-center font-weight-bold font-face-cocogoose">
@@ -85,8 +85,8 @@ export const planVacacional = () => {
           </h1>
           <h6 className="font-weight-light font-face-poppins-regular text-align-center">
             El Intensivo de Academia Tecnikids Venezuela tiene como concepto
-            formar a jovenes en el ambito de la robotica y programacion durante
-            el periodo vacacional para estudiantes de 13 a 18 años.
+            formar a jóvenes en el ámbito de la robótica y programación durante
+            el período vacacional para estudiantes de 13 a 18 años.
           </h6>
           <h6 className=" font-weight-light font-face-poppins-regular">
             Buscamos desarrollar las habilidades para el futuro, concentrándose
