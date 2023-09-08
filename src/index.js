3bc7eaa45e24f3225d6649dd89a3f011f4d15e41
jose0109 <martinezujd@gmail.com>
2023-09-08 19:59:23 -0400

    Redirect banner

--- src/index.js
+++ src/index.js
@@ -1,16 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './styles/index.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import * as serviceWorker from './serviceWorker';
-import { Layout } from './js/layout';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./styles/index.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import * as serviceWorker from "./serviceWorker";
+import { Layout } from "./js/layout";
 import "./styles/fonts/Neuropol-X-Rg.ttf";
+import banner from "./redirect.jpg";
 
 ReactDOM.render(
   <React.StrictMode>
-    <Layout />
+    {/* <Layout /> */}
+    <a className="banner" href="https://www.kuriosedu.com">
+      <img src="redirect.jpg" />
+    </a>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
